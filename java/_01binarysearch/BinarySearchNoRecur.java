e0f6339ef4bd6bce42380fd948caf77bca916460
WeasonTang <85794277+WeasonTang@users.noreply.github.com>
2024-07-26 23:03:42 +0800

    java_binarysearch not recursion

--- java/_01binarysearch/BinarySearchNoRecur.java
+++ java/_01binarysearch/BinarySearchNoRecur.java
@@ -2,6 +2,34 @@ package _01binarysearch;
 
 public class BinarySearchNoRecur {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        // test
+        int[] arr = { 1, 3, 8, 10, 11, 67, 100 };
+        System.out.println(binarySearch(arr, 100));
+
+    }
+
+    // 二分查找的非递归实现
+    /**
+     * 
+     * @param arr    待查找的数组arr
+     * @param target 需要查找的数
+     * @return 对应下标 -1 没找到
+     */
+    public static int binarySearch(int[] arr, int target) {
+
+        int left = 0;
+        int right = arr.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] > target) {
+                right = mid - 1; // 向左边查找
+            } else { // 向右查找
+                left = mid + 1;
+            }
+
+        }
+        return -1;
     }
 }
---
+++ java/java_algo.md
@@ -0,0 +1,5 @@
+## 1.二分查找
+
+
+
+<img src="assets/image-20240726211839879.png" alt="image-20240726211839879" style="zoom:67%;" />
